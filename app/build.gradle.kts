b1aafb6d44ec1560f96b1b7b7615e1934bcbb124
Gezim <gezimos@gmail.com>
2026-06-08 16:58:13 +0300

    Updated Build nr & settings screenshot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.gezimos.katapult"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
